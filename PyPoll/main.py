7e65cc2621322eb649e34db840bd22dfd4d4d160
ss728563 <71289207+ss728563@users.noreply.github.com>
2020-10-10 20:30:46 -0400

    update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,6 @@
 # Create a Python script that analyzes the votes and calculates each of the following:
 #  Total number of votes cast
-#  A complete list of candidates who received votes
+#  complete list of candidates who received votes
 #  The percentage of votes each candidate won
 #  The total number of votes each candidate won
 #  The winner of the election based on popular vote
